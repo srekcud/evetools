f6a1160c86783f76da07f3086d2a350ddde2076f
Sylvain Duckers <sylvain.duckers@gmail.com>
2026-02-16 17:50:53 +0100

    Fix TS build: cast Uint8Array to BufferSource for applicationServerKey

--- frontend/src/composables/usePushNotifications.ts
+++ frontend/src/composables/usePushNotifications.ts
@@ -47,7 +47,7 @@ export function usePushNotifications() {
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as BufferSource,
       })
 
       const store = useNotificationsStore()
